bdfb6e18b79e7eae3e928904fbe05dbd40a105f7
wisleypsicanalise-bit <wisleypsicanalise@gmail.com>
2026-09-25 16:51:46 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "jornada-da-fe-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
